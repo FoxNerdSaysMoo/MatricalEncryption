edc10b8a908e79303e1756664a06db18bbaebf8d
Zeb <zceboys@gmail.com>
2020-12-27 00:03:47 +0000

    Finished most C++ functionality

--- connection/CppEncryption.cpp
+++ connection/CppEncryption.cpp
@@ -1,20 +1,80 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <math.h>
 
 using namespace std;
 using Eigen::MatrixXd;
+using Eigen::MatrixXi;
 
-MatrixXd rand_gen(int dim, int max) {
+MatrixXi rand_gen(int dim, int max) {
     MatrixXd r = MatrixXd::Random(dim, dim);
-    return r * max;
+    return (r * max).cast<int>();
 }
 
+MatrixXi form_array(MatrixXi priv, MatrixXi shared, bool is_client) {
+    return (is_client)? priv * shared : shared * priv;
+}
+
+MatrixXi rm_det(MatrixXi matrix) {
+    matrix.row(0).setZero();
+    return matrix;
+}
+
+MatrixXi find_shared(MatrixXi priv, MatrixXi recv, bool is_client) {
+    return (is_client)? priv * recv : recv * priv;
+}
+
+MatrixXi add_det(MatrixXi matrix) {
+    matrix.row(0).setOnes();
+    return matrix;
+}
+
+MatrixXi encrypt_str(MatrixXi matrix, string str) {
+    int length = matrix.size() * ceil(float(str.length()) / matrix.size());
+
+    MatrixXi chars;
+    chars.resize(1, length);
+    chars.setZero();
+
+    for (int x = 0; x < str.length(); x++) {
+        chars(0, x) = int(str.at(x));
+    }
+
+    chars.resize(matrix.rows(), chars.size()/matrix.rows());
+    return matrix * chars;
+}
+
+string decrypt_str(MatrixXi shared, MatrixXi recv) {
+    MatrixXd chars = shared.cast<double>().inverse() * recv.cast<double>();
+    chars.resize(1, chars.size());
+
+    string result = "";
+
+    for (int x = 0; x < chars.size(); x++) {
+        if (int(chars(0, x)) == 0) { break; }
+        result += char(round(chars(0, x)));
+    }
+
+    return result;
+}
 
 int main() {
-    MatrixXd a, b, G;
+    MatrixXi a, b, G, Ga, Gb, bGa, aGb;
     a = rand_gen(5, 255);
     b = rand_gen(5, 255);
-    G = rand_gen(5, 255);
-    cout << a;
-}
+    G = rm_det(rand_gen(5, 255));
 
+    cout << "A:\n" << a << endl;
+
+    Ga = form_array(a, G, true);
+    Gb = form_array(b, G, false);
+
+    cout << "Ga:\n" << Ga << endl;
+
+    bGa = add_det(find_shared(b, Ga, false));
+    aGb = add_det(find_shared(a, Gb, true));
+
+    cout << "aGb - bGa:\n" << aGb - bGa << endl;
+
+    cout << decrypt_str(a, encrypt_str(a, "hello world"));
+}

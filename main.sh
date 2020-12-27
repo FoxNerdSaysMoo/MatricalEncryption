312459451233b472d6ab5b784064dac0d60a93a7
Zeb <zceboys@gmail.com>
2020-12-27 11:28:43 -0600

    Moved files to new folders

--- main.sh
+++ main.sh
@@ -1,5 +1,4 @@
-#python example.py
-#python connection/encrypt.py
-g++ -I /home/runner/PyEncryptors/eigen-3.3.9 connection/CppEncryption.cpp
+#python python_version/encrypt.py
+g++ -I /home/runner/PyEncryptors/eigen-3.3.9 cpp_version/CppEncryption.cpp
 ./a.out
 echo ''

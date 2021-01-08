8a097a6aa085d5bae0d364bf046450ce96448e71
zeb <zceboys@gmail.com>
2021-01-08 16:47:21 -0600

    Buffed gitignore and added binary read/write methods to cpp version

--- cpp_version/CppEncryption.cpp
+++ cpp_version/CppEncryption.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/Dense>
 #include <math.h>
 #include <time.h>
+#include <fstream>
 
 #define EIGEN_MPL2_ONLY
 
@@ -64,8 +65,27 @@ string decrypt_str(MatrixXi shared, MatrixXi recv) {
     return result;
 }
 
+void write_binary(const char* filename, const MatrixXi matrix) {
+    ofstream out(filename, ios::out | ios::binary | ios::trunc);
+    typename MatrixXi::Index rows=matrix.rows(), cols=matrix.cols();
+    out.write((char*) (&rows), sizeof(typename MatrixXi::Index));
+    out.write((char*) (&cols), sizeof(typename MatrixXi::Index));
+    out.write((char*) matrix.data(), rows * cols * sizeof(typename MatrixXi::Scalar) );
+    out.close();
+}
+
+void read_binary(const char* filename, MatrixXi& matrix) {
+    ifstream in(filename, ios::in | ios::binary);
+    typename MatrixXi::Index rows=0, cols=0;
+    in.read((char*) (&rows), sizeof(typename MatrixXi::Index));
+    in.read((char*) (&cols), sizeof(typename MatrixXi::Index));
+    matrix.resize(rows, cols);
+    in.read( (char *) matrix.data() , rows * cols * sizeof(typename MatrixXi::Scalar) );
+    in.close();
+}
+
 int main() {
-    MatrixXi a, b, G, Ga, Gb, bGa, aGb;
+    MatrixXi a, b, G, Ga, Gb, bGa, aGb, readaGb;
     a = rand_gen(5, 256);
     b = rand_gen(5, 256);
     G = rm_det(rand_gen(5, 256));
@@ -85,13 +105,18 @@ int main() {
     cout << decrypt_str(bGa, encrypt_str(bGa, "hello world")) << endl;
   
     string test_string = "";
-    for (int x = 0; x < 1048576; x++) { test_string += "."; }
+    for (int x = 0; x < (1048576/8); x++) { test_string += "."; }
   
     clock_t t = clock();
     decrypt_str(bGa, encrypt_str(aGb, test_string));
     t = clock() - t;
 
     cout << "Encryption/decryption time for 1mb was " << float(t)/CLOCKS_PER_SEC << "s" << endl;
+
+    write_binary("binary.dat", aGb);
+    read_binary("binary.dat", readaGb);
+
+    cout << readaGb - aGb << endl;
 
     return 0;
 }
--- main.sh
+++ main.sh
@@ -1,4 +1,3 @@
-python python_version/encrypt.py
-#g++ -I /home/runner/PyEncryptors/eigen-3.3.9 cpp_version/CppEncryption.cpp
-#./a.out
-echo ''
+#python python_version/encrypt.py
+g++ -I ./eigen-3.3.9/ cpp_version/CppEncryption.cpp
+./a.out
